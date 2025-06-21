ef575e0ab53de91956f9f73a5b3050327b9bba6d
F4k3r22 <fredyriveraacevedo13@gmail.com>
2025-06-21 14:32:59 -0600

    Updated examples of how to use Adapters

--- examples/adaptersexam.py
+++ examples/adaptersexam.py
@@ -1,21 +1,25 @@
 from app.adapters import BaseAdapter
 
-#adapter = BaseAdapter(
-#    name="Spring",
-#    command_init_tpl=[
-#        "java", "-jar", "{main_file}", ## We use the "{main_file}" to specify where the path of the 
+## Example that can be used Spring
+
+adapterJava = BaseAdapter(
+    name="Spring",
+    command_init_tpl=[
+        "java", "-jar", "{main_file}", ## We use the "{main_file}" to specify where the path of the 
                                         ##main file will be placed later when we want to launch the app, as well as with the host and port
-#        "server.address={host}",
-#        "server.port={port}"
-#    ],
-#    stop_command_tpl=[
-#        "java", "-jar", "{main_file}", "--stop" ## Same here
-#    ],
-#    config={"env": {"JAVA_OPTS": "-Xmx512m"}}
-#)
+        "server.address={host}",
+        "server.port={port}"
+    ],
+    stop_command_tpl=[
+        "java", "-jar", "{main_file}", "--stop" ## Same here
+    ],
+    config={"env": {"JAVA_OPTS": "-Xmx512m"}}
+)
 
-#adapter.register()
+adapterJava.register()
 
+
+## Example that can be used with JS Frameworks
 
 adapter_js = BaseAdapter(
     name="Vite2",
